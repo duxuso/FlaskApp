246bda031fb7a65efd293dcecf05a2c190679de5
duxuso <duxuso@gmail.com>
2015-09-06 20:26:57 +1000

    Commit Plus ++

--- app.py
+++ app.py
@@ -4,10 +4,18 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def main():
     # return "Welcome!"
     return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+@app.route('/signUp')
+def signUp():
+    # create user code will be here!!
 
 if __name__ == "__main__":
     app.run()
